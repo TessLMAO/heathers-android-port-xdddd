71bdc09e72703baab647a978afc753320f9542cc
TessLMAO <110305665+TessLMAO@users.noreply.github.com>
2022-11-03 17:51:17 -0300

    Add files via upload

---
+++ assets/preload/cinematic.lua
@@ -0,0 +1,13 @@
+
+bob = 0
+
+function onCreate() --CINIMATIC SCRIPT
+
+setPropertyFromClass('flixel.FlxG','sound.music.volume',0)
+setProperty('skipCountdown',true)
+setProperty('cpuControlled',true)
+setProperty('camHUD.visible',false)
+setProperty('camHUD.x',2000)
+
+end
+
